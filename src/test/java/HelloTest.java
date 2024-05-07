208ae602870a26c54ec8c72f676b9bd60d247a9a
Gulcan <glcngvncln@gmail.com>
2024-05-07 11:02:10 +0200

    Local repo

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,10 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello feature");
+
+        System.out.println(".idea is removed");
+
+        System.out.println("Local repo is already published");
+        System.out.println("Doing more work");
     }
 }
